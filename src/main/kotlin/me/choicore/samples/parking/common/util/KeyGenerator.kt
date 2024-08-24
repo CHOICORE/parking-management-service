31180e5070622b9e4e5c4263b373c6caafa4b965
CHOICORE <choijaehyeong93@gmail.com>
2024-08-24 17:54:47 +0900

    feat: add unique key generator with nano-id

---
+++ src/main/kotlin/me/choicore/samples/parking/common/util/KeyGenerator.kt
@@ -0,0 +1,15 @@
+package me.choicore.samples.parking.common.util
+
+import java.util.UUID.randomUUID
+
+interface KeyGenerator {
+    fun generate(): String
+}
+
+data object NanoIdKeyGenerator : KeyGenerator {
+    override fun generate(): String = NanoId.generate()
+}
+
+data object UUIDKeyGenerator : KeyGenerator {
+    override fun generate(): String = "${randomUUID()}"
+}
